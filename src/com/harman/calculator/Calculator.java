b438f559d4ba8616654ca5987e7a7c577ae746c7
sridharandharani <sridharandharani1815@gmail.com>
2022-03-16 12:57:00 +0530

    classes created

---
+++ src/com/harman/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.harman.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x,y;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Num 1 :");
+        x = input.nextInt();
+        System.out.println("Enter the Num 2 :");
+        y = input.nextInt();
+
+    }
+}
